b894b828ca4a02f72b941c00846cdab44ad141a4
palazard <alazard.pauline@gmail.com>
2023-04-18 17:16:24 +0800

    actualize database config

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
+      ssl: {rejectUnauthorized: false},
       searchPath: ['knex', 'public'],
-      ssl: true,
       host : process.env.DATABASE_HOST, //localhost
       user : process.env.DATABASE_USER, //add your user name for the database here
       port: process.env.DATABASE_PORT, // add your port number here
